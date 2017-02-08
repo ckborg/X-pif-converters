5ce07da4e4af40204904eb2ca6ec8d3c267f2fdb
Max Hutchinson <maxhutch@gmail.com>
2017-02-08 16:03:04 -0600

    Adding a bunch of boilerplate

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='x-pif-converters',
+    version='0.0.1',
+    url='http://github.com/CitrineInformatics/X-pif-converters',
+    description='Parsers to create PIFs from crystal characterization instruments',
+    author='Chris Borg',
+    author_email='cborg@citrine.io',
+    packages=find_packages(),
+    install_requires=[]
+)
