d75677969bca6591a5b9d83c8fac1bb3a7f7565d
Chris Borg <cborg@citrine.io>
2017-02-13 17:29:42 -0800

    renamed name to sparks_pif_converters

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='x-pif-converters',
+setup(name='sparks_pif_converters',
     version='0.0.1',
     url='http://github.com/CitrineInformatics/X-pif-converters',
     description='Parsers to create PIFs from crystal characterization instruments',
