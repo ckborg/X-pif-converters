36b361b454f8321132475b57c38fb2b4d1c68dc0
Chris Borg <cborg@citrine.io>
2017-03-08 14:20:11 -0800

    id should be first block of text before _ in file name

--- sparks_pif_converters/XRD/tests/test_raw_xrd.py
+++ sparks_pif_converters/XRD/tests/test_raw_xrd.py
@@ -4,5 +4,5 @@ from pypif.util.read_view import ReadView
 def test_raw_xrd():
     pif = raw_to_pif("./examples/XX001_TaPO5.raw")
     rv = ReadView(pif)
-    assert("TaPO5" in pif.ids)
+    assert("XX001" in pif.ids)
     assert(rv["2$\\theta$ (I$_{max}$)"].scalars[0].value == 21.530142749115)
